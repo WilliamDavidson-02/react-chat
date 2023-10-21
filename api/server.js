69cf3a0b7791bf85b2b5b5dd028e36b9f91819a3
William Davidson <wj.davidson@outlook.com>
2023-10-21 10:07:49 +0200

    server to api, vercel

--- api/server.js
+++ api/server.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
 const express = require("express");
-const { createClient } = require("@supabase/supabase-js");
+// const { createClient } = require("@supabase/supabase-js");
 const cors = require("cors");
 const app = express();
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+// const supabase = createClient(
+//   process.env.SUPABASE_URL,
+//   process.env.SUPABASE_KEY
+// );
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/server/register", (req, res) => {
+app.post("/api/register", (req, res) => {
   const { email, password } = req.body;
   res.status(200).json({ message: "test" });
 });
 
-app.post("/server/login", (req, res) => {
+app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
   res.status(200).json({ message: "test" });
 });
