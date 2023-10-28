e1820d9d33f50b6723398673a653eeaca64000c0
William Davidson <wj.davidson@outlook.com>
2023-10-28 06:52:23 +0200

    coloPallet

--- src/shared/Avatar.jsx
+++ src/shared/Avatar.jsx
@@ -1,15 +1,5 @@
 import React from "react";
-
-const colorPallet = [
-  "from-cyan-500 to-blue-500",
-  "from-emerald-400 to-blue-500",
-  "from-purple-500 to-blue-500",
-  "from-yellow-400 to-red-500",
-  "from-fuchsia-500 to-red-500",
-  "from-fuchsia-500 to-blue-500",
-  "from-lime-500 to-rose-500",
-  "from-violet-500 to-rose-500",
-];
+import { colorPallet } from './coloPallet';
 
 export default function Avatar({ profileImage }) {
   return (
---
+++ src/shared/coloPallet.js
@@ -0,0 +1,10 @@
+export const colorPallet = [
+    "from-cyan-500 to-blue-500",
+    "from-emerald-400 to-blue-500",
+    "from-purple-500 to-blue-500",
+    "from-yellow-400 to-red-500",
+    "from-fuchsia-500 to-red-500",
+    "from-fuchsia-500 to-blue-500",
+    "from-lime-500 to-rose-500",
+    "from-violet-500 to-rose-500",
+  ];
