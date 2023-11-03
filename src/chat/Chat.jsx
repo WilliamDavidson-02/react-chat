5bf200fb9dd51a531dfe456d2a95f435e5ced57f
William Davidson <wj.davidson@outlook.com>
2023-11-03 12:58:15 +0100

    awaiting friend accept view

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useRef, useState } from "react";
 import Sidebar from "./Sidebar";
 import SubmitBtn from "../shared/SubmitBtn";
 import { UserContext } from "../context/UserContext";
+import Profile from "./Profile";
 
 export default function Chat() {
   const { user } = useContext(UserContext);
@@ -36,7 +37,6 @@ export default function Chat() {
 
   function handleInputAreaChange(ev) {
     if (selectedFriend === null) return;
-    if (!user.friendList[selectedFriend].isFriend) return;
 
     setTextareaValue(ev.target.value);
   }
@@ -52,26 +52,50 @@ export default function Chat() {
         ref={messageRef}
         className="h-full pt-10 px-4 w-[calc(100%-240px)] flex flex-col justify-between"
       >
-        <div>Messages</div>
-        <form onSubmit={handleSubmit} className="flex items-end gap-2 py-4">
-          <textarea
-            ref={textareaRef}
-            value={textareaValue}
-            onChange={handleInputAreaChange}
-            onKeyDown={handleKeyDown}
-            placeholder="Type a message"
-            rows={1}
-            tabIndex={10}
-            className={`flex-grow px-4 py-4 outline-none rounded-lg resize-none bg-charcoal-gray-900 border border-charcoal-gray-600 focus:border-charcoal-gray-400 transition duration-300 max-h-[500px] ${
-              textareaRef.current?.scrollHeight >= 500
-                ? "overflow-y-scroll"
-                : "overflow-y-hidden"
-            }`}
-          />
-          <div className="max-h-20 h-full">
-            <SubmitBtn text={"Send"} />
+        {selectedFriend !== null ? (
+          user.friendList[selectedFriend].isFriend ? (
+            <>
+              <div>Messages</div>
+              <form
+                onSubmit={handleSubmit}
+                className="flex items-end gap-2 py-4"
+              >
+                <textarea
+                  ref={textareaRef}
+                  value={textareaValue}
+                  onChange={handleInputAreaChange}
+                  onKeyDown={handleKeyDown}
+                  placeholder="Type a message"
+                  rows={1}
+                  tabIndex={10}
+                  className={`flex-grow px-4 py-4 outline-none rounded-lg resize-none bg-charcoal-gray-900 border border-charcoal-gray-600 focus:border-charcoal-gray-400 transition duration-300 max-h-[500px] ${
+                    textareaRef.current?.scrollHeight >= 500
+                      ? "overflow-y-scroll"
+                      : "overflow-y-hidden"
+                  }`}
+                />
+                <div className="max-h-20 h-full">
+                  <SubmitBtn text={"Send"} />
+                </div>
+              </form>
+            </>
+          ) : (
+            <div className="w-full h-full flex flex-col items-center justify-center px-4">
+              <div className="mb-4">
+                <Profile user={user.friendList[selectedFriend]} />
+              </div>
+              <p className="text-charcoal-gray-400 text-center">
+                Awaiting {user.friendList[selectedFriend].firstName}{" "}
+                {user.friendList[selectedFriend].lastName} to accept your friend
+                request.
+              </p>
+            </div>
+          )
+        ) : (
+          <div className="w-full h-full flex justify-center items-center text-xl text-charcoal-gray-500">
+            &larr; Please select a user
           </div>
-        </form>
+        )}
       </div>
     </div>
   );
